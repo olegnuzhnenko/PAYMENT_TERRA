646585dcebe6279116622810a8684182b38e322d
olegnuzhnenko <82514158+olegnuzhnenko@users.noreply.github.com>
2023-03-20 00:52:22 +0100

    Add files via upload

--- PAYMENT_TERRA-main/index.js
+++ PAYMENT_TERRA-main/index.js
@@ -23,66 +23,152 @@ const onScroll = (event) => {
 document.addEventListener(`scroll`, onScroll)
 
 
-///на клик по кнопке apply now покрасить её в зеленый
+// ///на клик по кнопке apply now покрасить её в зеленый
 
-// const button = document.querySelector('button');
+// // const button = document.querySelector('button');
 
-// button.addEventListener('click', (event) => {
-//     const { target } = event;
-//     const buttonClicked = target;
-//     if (buttonClicked) {
-//         if (button.classList.contains("btn-apply")) {
-//             button.className = "btn-apply_active"
-//         } else if (button.classList.contains("btn-apply_active")) {
-//             button.className = ("btn-apply")
+// // button.addEventListener('click', (event) => {
+// //     const { target } = event;
+// //     const buttonClicked = target;
+// //     if (buttonClicked) {
+// //         if (button.classList.contains("btn-apply")) {
+// //             button.className = "btn-apply_active"
+// //         } else if (button.classList.contains("btn-apply_active")) {
+// //             button.className = ("btn-apply")
+// //         }
+// //     }
+// // })
+
+// ///на клик по ссылке в навигации переместить подчеркивание
+
+// // const allNavButtons = document.querySelectorAll(`.link`)
+
+// // allNavButtons.forEach((button) => {
+// //     button.addEventListener(`click`, (event) => {
+// //         event.preventDefault();
+// //         allNavButtons.forEach((button) => {
+// //             button.classList.remove(`link_active`);
+// //         })
+// //         const { target } = event;
+// //         target.classList.add(`link_active`);
+// //     })
+// // })
+
+
+
+// /////////////выезжающие дивы
+
+
+// // const headTitle = gsap.utils.toArray(`.title-text`);
+// const headGif = gsap.utils.toArray('.title-gif1');
+// const productsTitle = gsap.utils.toArray('.products-title');
+// const productsSubTitle = gsap.utils.toArray('.products-subtitle');
+// const cards = gsap.utils.toArray(`.card`);
+// const aboutText = gsap.utils.toArray(`.about-text`);
+// const aboutLogo = gsap.utils.toArray(`.myface1`);
+// // const stepsBox = gsap.utils.toArray(`.steps-box`);
+// // const stepsItems = gsap.utils.toArray(`.step`);
+// // const stepsButton = gsap.utils.toArray(`.step-button`);
+// const featuresTitle = gsap.utils.toArray(`.features-title`)
+// const featuresContent = gsap.utils.toArray(`.feature-container`);
+// const applySection = gsap.utils.toArray(`.process-blank`)
+// const applySectionSteps = gsap.utils.toArray(`.process-steps-wrapper`)
+// const subscribeTitle = gsap.utils.toArray(`.subscribe h2`)
+// const subscribeParagraph = gsap.utils.toArray(`.subscribe p`)
+// const subscribeForm = gsap.utils.toArray(`.sub-form`)
+// const map = gsap.utils.toArray(`.map`)
+
+// // headTitle.forEach(title => {
+// //     const anim = gsap.fromTo(
+// //         title, {
+// //             autoAlpha: 0,
+// //             y: -800,
+// //             x: 0,
+// //             rotate: 0
+// //         }, {
+// //             duration: 1.5,
+// //             autoAlpha: 1,
+// //             y: 0,
+// //             x: 0,
+// //             rotate: 0
+// //         }
+// //     );
+// //     ScrollTrigger.create({
+// //         trigger: title,
+// //         animation: anim,
+// //     });
+// // });
+
+// headGif.forEach(gif => {
+//     const anim = gsap.fromTo(
+//         gif, {
+//             autoAlpha: 0,
+//             y: 0,
+//             x: 900,
+//             rotate: 0
+//         }, {
+//             duration: 1,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
 //         }
-//     }
-// })
+//     );
+//     ScrollTrigger.create({
+//         trigger: gif,
+//         animation: anim,
+//     });
+// });
 
-///на клик по ссылке в навигации переместить подчеркивание
+// aboutLogo.forEach(logo => {
+//     const anim = gsap.fromTo(
+//         logo, {
+//             autoAlpha: 0,
+//             y: 300,
+//             x: 0,
+//             rotate: 0
+//         }, {
+//             duration: 1,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
+//         }
+//     );
+//     ScrollTrigger.create({
+//         trigger: logo,
+//         animation: anim,
+//     });
+// });
 
-// const allNavButtons = document.querySelectorAll(`.link`)
 
-// allNavButtons.forEach((button) => {
-//     button.addEventListener(`click`, (event) => {
-//         event.preventDefault();
-//         allNavButtons.forEach((button) => {
-//             button.classList.remove(`link_active`);
-//         })
-//         const { target } = event;
-//         target.classList.add(`link_active`);
-//     })
-// })
+// aboutText.forEach(text => {
+//     const anim = gsap.fromTo(
+//         text, {
+//             autoAlpha: 0,
+//             y: 0,
+//             x: -500,
+//             rotate: 0
+//         }, {
+//             duration: 1.5,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
+//         }
+//     );
+//     ScrollTrigger.create({
+//         trigger: text,
+//         animation: anim,
+//     });
+// });
 
 
-
-/////////////выезжающие дивы
-
-
-// const headTitle = gsap.utils.toArray(`.title-text`);
-const headGif = gsap.utils.toArray('.title-gif1');
-const productsTitle = gsap.utils.toArray('.products-title');
-const productsSubTitle = gsap.utils.toArray('.products-subtitle');
-const cards = gsap.utils.toArray(`.card`);
-const aboutText = gsap.utils.toArray(`.about-text`);
-const aboutLogo = gsap.utils.toArray(`.myface1`);
-// const stepsBox = gsap.utils.toArray(`.steps-box`);
-// const stepsItems = gsap.utils.toArray(`.step`);
-// const stepsButton = gsap.utils.toArray(`.step-button`);
-const featuresTitle = gsap.utils.toArray(`.features-title`)
-const featuresContent = gsap.utils.toArray(`.feature-container`);
-const applySection = gsap.utils.toArray(`.process-blank`)
-const applySectionSteps = gsap.utils.toArray(`.process-steps-wrapper`)
-const subscribeTitle = gsap.utils.toArray(`.subscribe h2`)
-const subscribeParagraph = gsap.utils.toArray(`.subscribe p`)
-const subscribeForm = gsap.utils.toArray(`.sub-form`)
-const map = gsap.utils.toArray(`.map`)
-
-// headTitle.forEach(title => {
+// productsTitle.forEach(title => {
 //     const anim = gsap.fromTo(
 //         title, {
 //             autoAlpha: 0,
-//             y: -800,
+//             y: 200,
 //             x: 0,
 //             rotate: 0
 //         }, {
@@ -99,145 +185,147 @@ const map = gsap.utils.toArray(`.map`)
 //     });
 // });
 
-headGif.forEach(gif => {
-    const anim = gsap.fromTo(
-        gif, {
-            autoAlpha: 0,
-            y: 0,
-            x: 900,
-            rotate: 0
-        }, {
-            duration: 1,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: gif,
-        animation: anim,
-    });
-});
-
-aboutLogo.forEach(logo => {
-    const anim = gsap.fromTo(
-        logo, {
-            autoAlpha: 0,
-            y: 300,
-            x: 0,
-            rotate: 0
-        }, {
-            duration: 1,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: logo,
-        animation: anim,
-    });
-});
-
-
-aboutText.forEach(text => {
-    const anim = gsap.fromTo(
-        text, {
-            autoAlpha: 0,
-            y: 0,
-            x: -500,
-            rotate: 0
-        }, {
-            duration: 1.5,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: text,
-        animation: anim,
-    });
-});
-
-
-productsTitle.forEach(title => {
-    const anim = gsap.fromTo(
-        title, {
-            autoAlpha: 0,
-            y: 200,
-            x: 0,
-            rotate: 0
-        }, {
-            duration: 1.5,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: title,
-        animation: anim,
-    });
-});
-
-productsSubTitle.forEach(title => {
-    const anim = gsap.fromTo(
-        title, {
-            autoAlpha: 0,
-            y: 200,
-            x: 0,
-            rotate: 0
-        }, {
-            duration: 1.5,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: title,
-        animation: anim,
-    });
-});
+// productsSubTitle.forEach(title => {
+//     const anim = gsap.fromTo(
+//         title, {
+//             autoAlpha: 0,
+//             y: 200,
+//             x: 0,
+//             rotate: 0
+//         }, {
+//             duration: 1.5,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
+//         }
+//     );
+//     ScrollTrigger.create({
+//         trigger: title,
+//         animation: anim,
+//     });
+// });
 
 
 
-cards.forEach(card => {
-    const anim = gsap.fromTo(
-        card, {
-            autoAlpha: 0,
-            y: 200,
-            x: 0,
-            rotate: 0
-        }, {
-            duration: 1,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: card,
-        animation: anim,
-    });
-});
+// cards.forEach(card => {
+//     const anim = gsap.fromTo(
+//         card, {
+//             autoAlpha: 0,
+//             y: 200,
+//             x: 0,
+//             rotate: 0
+//         }, {
+//             duration: 1,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
+//         }
+//     );
+//     ScrollTrigger.create({
+//         trigger: card,
+//         animation: anim,
+//     });
+// });
 
-// stepsBox.forEach(step => {
+// // stepsBox.forEach(step => {
+// //     const anim = gsap.fromTo(
+// //         step, {
+// //             autoAlpha: 0,
+// //             y: 0,
+// //             x: -600,
+// //             rotate: 0
+// //         }, {
+// //             duration: 1,
+// //             autoAlpha: 1,
+// //             y: 0,
+// //             x: 0,
+// //             rotate: 0
+// //         }
+// //     );
+// //     ScrollTrigger.create({
+// //         trigger: step,
+// //         animation: anim,
+// //     });
+// // });
+
+// // stepsItems.forEach(item => {
+// //     const anim = gsap.fromTo(
+// //         item, {
+// //             autoAlpha: 0,
+// //             y: 0,
+// //             x: -50,
+// //             rotate: 0
+// //         }, {
+// //             duration: 2,
+// //             autoAlpha: 1,
+// //             y: 0,
+// //             x: 0,
+// //             rotate: 0
+// //         }
+// //     );
+// //     ScrollTrigger.create({
+// //         trigger: item,
+// //         animation: anim,
+// //     });
+// // });
+
+
+
+// // stepsButton.forEach(button => {
+// //     const anim = gsap.fromTo(
+// //         button, {
+// //             autoAlpha: 0,
+// //             y: 100,
+// //             x: 0,
+// //             rotate: 0
+// //         }, {
+// //             duration: 0.2,
+// //             autoAlpha: 1,
+// //             y: 0,
+// //             x: 0,
+// //             rotate: 0
+// //         }
+// //     );
+// //     ScrollTrigger.create({
+// //         trigger: button,
+// //         animation: anim,
+// //     });
+// // });
+
+
+// applySection.forEach(section => {
+//     const anim = gsap.fromTo(
+//         section, {
+//             autoAlpha: 0,
+//             y: 0,
+//             x: -700,
+//             rotate: 0
+//         }, {
+//             duration: 1.5,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
+//         }
+//     );
+//     ScrollTrigger.create({
+//         trigger: section,
+//         animation: anim,
+//     });
+// });
+
+
+// applySectionSteps.forEach(step => {
 //     const anim = gsap.fromTo(
 //         step, {
 //             autoAlpha: 0,
 //             y: 0,
-//             x: -600,
+//             x: 700,
 //             rotate: 0
 //         }, {
-//             duration: 1,
+//             duration: 1.5,
 //             autoAlpha: 1,
 //             y: 0,
 //             x: 0,
@@ -250,15 +338,15 @@ cards.forEach(card => {
 //     });
 // });
 
-// stepsItems.forEach(item => {
+// subscribeTitle.forEach(title => {
 //     const anim = gsap.fromTo(
-//         item, {
+//         title, {
 //             autoAlpha: 0,
-//             y: 0,
-//             x: -50,
+//             y:100,
+//             x: 0,
 //             rotate: 0
 //         }, {
-//             duration: 2,
+//             duration: 1,
 //             autoAlpha: 1,
 //             y: 0,
 //             x: 0,
@@ -266,22 +354,21 @@ cards.forEach(card => {
 //         }
 //     );
 //     ScrollTrigger.create({
-//         trigger: item,
+//         trigger: title,
 //         animation: anim,
 //     });
 // });
 
 
-
-// stepsButton.forEach(button => {
+// subscribeParagraph.forEach(title => {
 //     const anim = gsap.fromTo(
-//         button, {
+//         title, {
 //             autoAlpha: 0,
-//             y: 100,
+//             y: 200,
 //             x: 0,
 //             rotate: 0
 //         }, {
-//             duration: 0.2,
+//             duration: 1,
 //             autoAlpha: 1,
 //             y: 0,
 //             x: 0,
@@ -289,139 +376,52 @@ cards.forEach(card => {
 //         }
 //     );
 //     ScrollTrigger.create({
-//         trigger: button,
+//         trigger: title,
 //         animation: anim,
 //     });
 // });
 
+// subscribeForm.forEach(title => {
+//     const anim = gsap.fromTo(
+//         title, {
+//             autoAlpha: 0,
+//             y: 300,
+//             x: 0,
+//             rotate: 0
+//         }, {
+//             duration: 1.5,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
+//         }
+//     );
+//     ScrollTrigger.create({
+//         trigger: title,
+//         animation: anim,
+//     });
+// });
 
-applySection.forEach(section => {
-    const anim = gsap.fromTo(
-        section, {
-            autoAlpha: 0,
-            y: 0,
-            x: -700,
-            rotate: 0
-        }, {
-            duration: 1.5,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: section,
-        animation: anim,
-    });
-});
-
-
-applySectionSteps.forEach(step => {
-    const anim = gsap.fromTo(
-        step, {
-            autoAlpha: 0,
-            y: 0,
-            x: 700,
-            rotate: 0
-        }, {
-            duration: 1.5,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: step,
-        animation: anim,
-    });
-});
-
-subscribeTitle.forEach(title => {
-    const anim = gsap.fromTo(
-        title, {
-            autoAlpha: 0,
-            y:100,
-            x: 0,
-            rotate: 0
-        }, {
-            duration: 1,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: title,
-        animation: anim,
-    });
-});
-
-
-subscribeParagraph.forEach(title => {
-    const anim = gsap.fromTo(
-        title, {
-            autoAlpha: 0,
-            y: 200,
-            x: 0,
-            rotate: 0
-        }, {
-            duration: 1,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: title,
-        animation: anim,
-    });
-});
-
-subscribeForm.forEach(title => {
-    const anim = gsap.fromTo(
-        title, {
-            autoAlpha: 0,
-            y: 300,
-            x: 0,
-            rotate: 0
-        }, {
-            duration: 1.5,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: title,
-        animation: anim,
-    });
-});
-
-map.forEach(title => {
-    const anim = gsap.fromTo(
-        title, {
-            autoAlpha: 0,
-            y: 0,
-            x: 1000,
-            rotate: 0
-        }, {
-            duration: 1.5,
-            autoAlpha: 1,
-            y: 0,
-            x: 0,
-            rotate: 0
-        }
-    );
-    ScrollTrigger.create({
-        trigger: title,
-        animation: anim,
-    });
-});
+// map.forEach(title => {
+//     const anim = gsap.fromTo(
+//         title, {
+//             autoAlpha: 0,
+//             y: 0,
+//             x: 1000,
+//             rotate: 0
+//         }, {
+//             duration: 1.5,
+//             autoAlpha: 1,
+//             y: 0,
+//             x: 0,
+//             rotate: 0
+//         }
+//     );
+//     ScrollTrigger.create({
+//         trigger: title,
+//         animation: anim,
+//     });
+// });
 
 /////FORM CHECKER
 
